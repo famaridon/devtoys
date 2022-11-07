b260d61b2a79b53c8ff0a23687e8d988159d8632
famaridon <famaridon@users.noreply.github.com>
2022-11-07 09:03:26 +0100

    fix GA

--- src/app/core/google-analytics.service.ts
+++ src/app/core/google-analytics.service.ts
@@ -1,26 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs';
+import { delay, filter, map, timer } from 'rxjs';
 
 declare const gtag: any;
+declare const gtagError: boolean | undefined;
 
 @Injectable({
   providedIn: 'root',
 })
 export class GoogleAnalyticsService {
   public constructor(private _router: Router) {
-    debugger;
     this._router.events
       .pipe(
         filter((event) => event instanceof NavigationEnd),
         map((event) => event as NavigationEnd)
       )
       .subscribe((event: NavigationEnd) => {
-        /** START : Code to Track Page View  */
         gtag('event', 'page_view', {
           page_path: event.urlAfterRedirects,
         });
-        /** END */
       });
   }
 }
