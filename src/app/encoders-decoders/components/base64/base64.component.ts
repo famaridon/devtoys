c27ec0886ac327f8d3e4e41be601ac117f93fd88
famaridon <famaridon@users.noreply.github.com>
2022-11-10 23:01:50 +0100

    store base64 settings into local storage to remember las value

--- src/app/encoders-decoders/components/base64/base64.component.ts
+++ src/app/encoders-decoders/components/base64/base64.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs';
+import { LocalStorageService } from 'src/app/core/local-storage.service';
 import { Base64Service } from 'src/app/encoders-decoders/services/base64.service';
+
+const DEFAULT_BASE64_PREFERENCE: Base64Preference = {
+  encode: true,
+  input: '',
+};
 
 @Component({
   selector: 'app-base64',
@@ -27,9 +33,23 @@ export class Base64Component {
 
   public output: string = '';
 
-  public constructor(private base64Service: Base64Service) {}
+  public constructor(
+    private base64Service: Base64Service,
+    private localStorageService: LocalStorageService
+  ) {
+    this.localStorageService
+      .getItem<Base64Preference>('base64', DEFAULT_BASE64_PREFERENCE)
+      .subscribe((pref) => {
+        this._encode = pref.encode;
+        this._input = pref.input;
+      });
+  }
 
   private somethingChanged(): void {
+    this.localStorageService.setItem<Base64Preference>('base64', {
+      encode: this._encode,
+      input: this._input,
+    });
     if (this.encode) {
       this.base64Service
         .encode(this._input)
@@ -43,3 +63,8 @@ export class Base64Component {
     }
   }
 }
+
+type Base64Preference = {
+  encode: boolean;
+  input: string;
+};
