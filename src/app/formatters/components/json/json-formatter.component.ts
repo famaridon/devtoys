2e0dcd4068f27b257c9b1c64ac2ff5950c6de6fd
famaridon <famaridon@users.noreply.github.com>
2022-11-14 22:38:33 +0100

    allow to chnage indent format

--- src/app/formatters/components/json/json-formatter.component.ts
+++ src/app/formatters/components/json/json-formatter.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { JsonService } from 'src/app/formatters/services/json.service';
+import { LocalStorageService } from 'src/app/core/local-storage.service';
+import {
+  IndentMode,
+  JsonService,
+} from 'src/app/formatters/services/json.service';
+
+type JsonFormatterPreference = {
+  indentMode: IndentMode;
+};
+const PREFERENCE_KEY = 'jsonFormatter';
+
+const DEFAULT_PREFERENCE: JsonFormatterPreference = {
+  indentMode: '2-spaces',
+};
 
 @Component({
   selector: 'app-json-formatter',
@@ -7,23 +20,19 @@ import { JsonService } from 'src/app/formatters/services/json.service';
 })
 export class JsonFormatterComponent implements OnInit {
   public error: string | null = null;
+  private _indentMode: IndentMode = '2-spaces';
+  public get indentMode(): IndentMode {
+    return this._indentMode;
+  }
+  public set indentMode(value: IndentMode) {
+    this._indentMode = value;
+    this.somthingChanged();
+  }
 
   private _input: string = '';
   public set input(input: string) {
     this._input = input;
-    this.jsonService.formatte(input).subscribe({
-      next: (output) => {
-        this.error = null;
-        this.output = output;
-      },
-      error: (error: string | Error) => {
-        if (error instanceof Error) {
-          this.error = error.message;
-        } else {
-          this.error = error;
-        }
-      },
-    });
+    this.somthingChanged();
   }
 
   public get input(): string {
@@ -32,9 +41,39 @@ export class JsonFormatterComponent implements OnInit {
 
   public output: string = '';
 
-  public constructor(private jsonService: JsonService) {}
+  public constructor(
+    private jsonService: JsonService,
+    private localStorageService: LocalStorageService
+  ) {
+    this.localStorageService
+      .getItem<JsonFormatterPreference>(PREFERENCE_KEY, DEFAULT_PREFERENCE)
+      .subscribe((pref) => {
+        this._indentMode = pref.indentMode;
+      });
+  }
 
   public ngOnInit(): void {
     this.input = '{\n}';
+  }
+
+  private somthingChanged(): void {
+    this.localStorageService.setItem<JsonFormatterPreference>(PREFERENCE_KEY, {
+      indentMode: this._indentMode,
+    });
+    this.jsonService
+      .formatte(this._input, { mode: this._indentMode })
+      .subscribe({
+        next: (output) => {
+          this.error = null;
+          this.output = output;
+        },
+        error: (error: string | Error) => {
+          if (error instanceof Error) {
+            this.error = error.message;
+          } else {
+            this.error = error;
+          }
+        },
+      });
   }
 }
--- src/app/formatters/services/json.service.ts
+++ src/app/formatters/services/json.service.ts
@@ -2,15 +2,34 @@ import { Injectable } from '@angular/core';
 import { from, Observable } from 'rxjs';
 import { FormattersModule } from 'src/app/formatters/formatters.module';
 
+const DEFAULT_INDENT_OPTIONS: IndentOptions = { mode: '2-spaces' };
+
 @Injectable()
 export class JsonService {
   public constructor() {}
 
-  public formatte(raw: string): Observable<string> {
-    return from(this.f(raw));
+  public formatte(
+    raw: string,
+    options?: Partial<IndentOptions>
+  ): Observable<string> {
+    return from(this.f(raw, { ...DEFAULT_INDENT_OPTIONS, ...options }));
   }
 
-  private async f(raw: string): Promise<string> {
-    return await JSON.stringify(JSON.parse(raw), null, 2);
+  private async f(raw: string, options: IndentOptions): Promise<string> {
+    let indent: string | number;
+    if (options.mode === 'tabs') {
+      indent = '\t';
+    } else if (options.mode === '4-spaces') {
+      indent = 4;
+    } else {
+      indent = 2;
+    }
+    return await JSON.stringify(JSON.parse(raw), null, indent);
   }
 }
+
+export type IndentOptions = {
+  mode: IndentMode;
+};
+
+export type IndentMode = '2-spaces' | '4-spaces' | 'tabs';
